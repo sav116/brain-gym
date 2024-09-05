6517dd8d25b7f165209a0bc5c5ebdd35422b27eb
Соловьев Артём Витальевич <art.poker@mail.ru>
2024-09-05 15:18:47 +0700

    Uniform number generation with 1 to 4 digits

--- keyboards/inlinekeyboards.py
+++ keyboards/inlinekeyboards.py
@@ -2,13 +2,19 @@ from aiogram.types import InlineKeyboardMarkup, InlineKeyboardButton
 import random
 
 def create_math_keyboard():
-    a = random.randint(10, 1000)
-    b = random.randint(10, 1000)
+    digits_a = random.randint(1, 4)
+    digits_b = random.randint(1, 4)
+    
+    a = random.randint(10**(digits_a - 1), 10**digits_a - 1)
+    b = random.randint(10**(digits_b - 1), 10**digits_b - 1)
+    
     operations = ['+', '-']
     operation = random.choice(operations)
     question = f"{a} {operation} {b} = ?"
+    
     keyboard = InlineKeyboardMarkup(resize_keyboard=True)
     button_check = InlineKeyboardButton(text="Check", callback_data=f"check_result:{a}:{operation}:{b}")
     button_next = InlineKeyboardButton(text="Next", callback_data="next_question")
     keyboard.add(button_check, button_next)
+    
     return question, keyboard
